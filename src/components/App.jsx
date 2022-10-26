f7b21e60250c413d6d36ec689a0cb368270dbf7f
CarmenViolet <margaretholmes200@gmail.com>
2022-10-26 20:45:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Layout } from './Layout/Layout';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
+import { Loader } from './Loader/Loader';
 
 const Movies = lazy(() => import('./pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('./pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('./pages/Cast/Cast'));
 const Reviews = lazy(() => import('./pages/Reviews/Reviews'));
 
-
 export const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
